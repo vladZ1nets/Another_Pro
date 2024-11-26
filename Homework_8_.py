12f13c26329a9058eaa6151f5df7fcd932e90fbb
vladZ1nets <vlad.zinets@gmail.com>
2024-11-26 14:07:09 +0200

    Update Homework_8_.py

--- Homework_8_.py
+++ Homework_8_.py
@@ -1,6 +1,6 @@
 import datetime
-from multiprocessing import Queue
-from multiprocessing import Process
+from multiprocessing import Queue, Process
+from multiprocessing import Pool
 
 
 def collatz_sequence(n):
@@ -12,49 +12,40 @@ def collatz_sequence(n):
     return True
 
 
-def part_count(from_number, to_number, queue_object):
+def part_count(from_number, to_number):
     count = 0
     for i in range(from_number, to_number):
         if collatz_sequence(i):
             count += 1
-    queue_object.put(count)
+    return count
+
+
+def worker_task(task_range):
+    from_number, to_number = task_range
+    return part_count(from_number, to_number)
 
 
 if __name__ == '__main__':
-    numbers_queue = Queue()
     num_processes = 4
     range_limit = 1000000000
+    chunk_size = range_limit // num_processes
 
-    t1 = Process(target=part_count, args=(1, range_limit // num_processes, numbers_queue))
-    t2 = Process(target=part_count,
-                 args=(range_limit // num_processes, 2 * range_limit // num_processes, numbers_queue))
-    t3 = Process(target=part_count,
-                 args=(2 * range_limit // num_processes, 3 * range_limit // num_processes, numbers_queue))
-    t4 = Process(target=part_count, args=(3 * range_limit // num_processes, range_limit, numbers_queue))
+    task_ranges = [(i * chunk_size, (i + 1) * chunk_size) for i in range(num_processes)]
 
     print("Counting ...")
     print("Please wait ...")
 
     start_time = datetime.datetime.now()
 
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
+    with Pool(processes=num_processes) as pool:
+        results = pool.map(worker_task, task_ranges)
 
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
+    total_count = sum(results)
 
     end_time = datetime.datetime.now()
     print("Total time: ", end_time - start_time)
 
-    total_count = 0
-    while not numbers_queue.empty():
-        total_count += numbers_queue.get()  
-
-    print(f"Total numbers that passed the Collatz hypothesis check: {total_count}")
+    print(f"Total numbers that passed Collatz hypothesis: {total_count}")
 
 
 
